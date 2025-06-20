7f1b1ca187cfb7ec75888709b321463372e2f124
Satyam Kandpal <satyamkandpal8@gmail.com>
2025-06-20 23:57:36 +0530

    for comments-container (commentsData)

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,43 +1,43 @@
 const commentsData = [
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [],
   },
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [
       {
-        name: "Akshay Saini",
+        name: "Satyam Kandpal",
         text: "Lorem ipsum dolor sit amet, consectetur adip",
         replies: [],
       },
       {
-        name: "Akshay Saini",
+        name: "Satyam Kandpal",
         text: "Lorem ipsum dolor sit amet, consectetur adip",
         replies: [
           {
-            name: "Akshay Saini",
+            name: "Satyam Kandpal",
             text: "Lorem ipsum dolor sit amet, consectetur adip",
             replies: [
               {
-                name: "Akshay Saini",
+                name: "Satyam Kandpal",
                 text: "Lorem ipsum dolor sit amet, consectetur adip",
                 replies: [
                   {
-                    name: "Akshay Saini",
+                    name: "Satyam Kandpal",
                     text: "Lorem ipsum dolor sit amet, consectetur adip",
                     replies: [
                       {
-                        name: "Akshay Saini",
+                        name: "Satyam Kandpal",
                         text: "Lorem ipsum dolor sit amet, consectetur adip",
                         replies: [],
                       },
                     ],
                   },
                   {
-                    name: "Akshay Saini",
+                    name: "Satyam Kandpal",
                     text: "Lorem ipsum dolor sit amet, consectetur adip",
                     replies: [],
                   },
@@ -50,22 +50,22 @@ const commentsData = [
     ],
   },
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [],
   },
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [],
   },
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [],
   },
   {
-    name: "Akshay Saini",
+    name: "Satyam Kandpal",
     text: "Lorem ipsum dolor sit amet, consectetur adip",
     replies: [],
   },
