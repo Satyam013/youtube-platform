d8087a3f9b7bbe726bbcce2820f8cc0bec04285a
Satyam Kandpal <satyamkandpal8@gmail.com>
2025-06-20 23:55:25 +0530

    Search Bar using debouncing to optimise performance of searching while making API calls.

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,12 +1,69 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  console.log(searchQuery);
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
+  const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
+
+  /**
+   *  searchCache = {
+   *      "iphone" : ["iphone 11", "iphone 12"]
+   *  }
+   *  searchQuery = iphone
+   */
+
+  useEffect(() => {
+    // API call
+    // make an api call after ever key press
+    // but if the difference between 2 APIs calls is <200ms
+    // decline the API call
+
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestion();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  /**
+   * key - i
+   *  - render the component
+   *  - useEffect();
+   *  - start timer => make api call after 200ms
+
+   * key - ip
+   *  - destroy the component (useEffect return method)
+   *  - re-render the component
+   *  - useEffect()
+   *  - start timer => make api call after 200 ms
+   */
+
+  const getSearchSuggestion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    // console.log(json[1]);
+    setSuggestions(json[1]);
+
+    // update cache
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -14,6 +71,7 @@ const Head = () => {
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      {/* LOGO Section*/}
       <div className="flex col-span-1">
         <img
           onClick={() => toggleMenuHandler()}
@@ -28,19 +86,38 @@ const Head = () => {
         />
       </div>
 
-      <div className="col-span-10 flex justify-center">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          🔍
-        </button>
+      {/* Search Bar  */}
+      <div className="col-span-7 relative">
+        <div className="flex justify-center">
+          <input
+            className="w-full border border-gray-400 p-2 px-4 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search"
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            🔍
+          </button>
+        </div>
+        {/* Show Suggestion  */}
+        {showSuggestions && (
+          <div className="absolute bg-white py-2 px-2 w-full shadow-lg rounded-lg border mt-1 z-10">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
 
-      <div>
+      {/* User Icon */}
+      <div className="col-span-3 flex justify-end pr-5">
         <img
           className="h-8"
           alt="user"
