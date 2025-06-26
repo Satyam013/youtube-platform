a9ce27171286e6395337b975e4d0a3c42d01c963
Satyam Kandpal <satyamkandpal8@gmail.com>
2025-06-27 04:07:18 +0530

    searchResults update commit

--- src/components/Header/SearchText/SearchResults.js
+++ src/components/Header/SearchText/SearchResults.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import VideoCardTrending from "../../VideoCards/VideoCardTrending";
+import { Link } from "react-router-dom";
 
 const SearchResults = () => {
   const { results, query } = useSelector((store) => store.searchResult);
@@ -12,7 +13,12 @@ const SearchResults = () => {
       </h2>
       <div className="flex flex-wrap gap-4">
         {results?.map((video) => (
-          <VideoCardTrending key={video.id?.videoId || video.id} info={video} />
+          <Link key={video.id.videoId} to={`/watch?v=${video.id.videoId}`}>
+            <VideoCardTrending
+              key={video.id?.videoId || video.id}
+              info={video}
+            />
+          </Link>
         ))}
       </div>
     </div>
